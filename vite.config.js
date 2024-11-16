6cf1cf89e707bb4c9c6977eceb7a5c37b89ee083
arbinzaman <arbinzmn@gmail.com>
2024-11-17 02:55:13 +0600

    fix: second attemp to fix this bug regarding to vercel

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Enable hot reload
-    hmr: true,
-  },
-  build: {
-    // Optimize dependencies
-    commonjsOptions: {
-      include: [/react-router-dom/],
-    },
-  },
-  // Experimental settings to control React behavior with Vite
-  define: {
-    'process.env': {},
-    'REACT_APP_ROUTER_V6': true,
-  },
-  optimizeDeps: {
-    include: ['react-router-dom'],
-  },
-});
+})
