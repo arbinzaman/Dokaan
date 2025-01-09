bd6cdfa96afa15ffe7803e7b03e314f16f2f9a5d
arbinzaman <arbinzmn@gmail.com>
2025-01-09 12:39:24 +0600

    fix: Added Optional chaining

--- src/components/dashBoard/home/settings/Profile.jsx
+++ src/components/dashBoard/home/settings/Profile.jsx
@@ -3,19 +3,19 @@ import SettingSection from "./SettingSection";
 import { useUser } from "../../../../contexts/AuthContext";
 const Profile = () => {
 	const{user} = useUser();
-	console.log(user.user.email);
+	console.log(user?.user?.email);
 	return (
 		<SettingSection icon={User} title={"Profile"}>
 			<div className='flex flex-col sm:flex-row items-center mb-6'>
 				<img
-					src={user.user.profileImageUrl}
+					src={user?.user?.profileImageUrl}
 					alt='Profile Picture'
 					className='rounded-full w-20 h-20 object-cover mr-4'
 				/>
 
 				<div>
-					<h3 className='text-lg font-semibold text-gray-100'>{user.user.name}</h3>
-					<p className='text-gray-400'>{user.user.email}</p>
+					<h3 className='text-lg font-semibold text-gray-100'>{user?.user?.name}</h3>
+					<p className='text-gray-400'>{user?.user?.email}</p>
 				</div>
 			</div>
 
