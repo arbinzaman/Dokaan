b1b4f0c90961ebd56557a8a9e16a5b52bf569ad3
arbinzaman <arbinzmn@gmail.com>
2025-02-02 02:11:11 +0600

    style: landingPage: fixed more design issues

--- src/constants/home/social.js
+++ src/constants/home/social.js
@@ -1,41 +1,38 @@
-import {
-    discordBlack,
-    facebook,
-    instagram,
-    telegram,
-    twitter,
-  } from "../../assets";
-  
+import { IoLogoDiscord } from "react-icons/io5";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+
 export const socials = [
     {
       id: "0",
       title: "Discord",
-      icon: discordBlack,
-      url: "#",
+      icon: IoLogoDiscord,
+      url: "https://discord.gg/NKWzHfjCM8",
     },
     {
       id: "1",
-      title: "Twitter",
-      icon: twitter,
-      url: "#",
+      title: "LinkedIn",
+      icon: FaLinkedin,
+      url: "https://www.linkedin.com/company/dokaaan/",
     },
     {
       id: "2",
       title: "Instagram",
-      icon: instagram,
+      icon: FaInstagram,
       url: "#",
     },
     {
       id: "3",
       title: "Telegram",
-      icon: telegram,
+      icon: FaTelegramPlane,
       url: "#",
     },
     {
       id: "4",
       title: "Facebook",
-      icon: facebook,
-      url: "#",
+      icon: FaFacebook,
+      url: "https://www.facebook.com/people/Dokaan/61572401851789/",
     },
-  ];
-  
+];
