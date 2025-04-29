0b7aa2eabe38c4dfbbb757df8b06f0343dfda0b9
Arbin Zaman <arbin16203@anwargroup.com>
2025-04-29 13:22:08 +0600

    style: fixed vercel error issue 3

--- src/components/landingPage/home/Benefits.jsx
+++ src/components/landingPage/home/Benefits.jsx
@@ -3,7 +3,7 @@ import Heading from "./Heading";
 import Section from "./Section";
 // import Arrow from "../../../assets/home/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
-import ClipPath from "../../../assets/home/svg/ClipPath";
+// import ClipPath from "../../../assets/home/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -61,7 +61,11 @@ const Benefits = () => {
                 </div>
               </div>
 
-              <ClipPath />
+              <svg className="block" width={0} height={0}>
+                <clipPath id="benefits" clipPathUnits="objectBoundingBox">
+                  <path d="M0.079,0 h0.756 a0.079,0.083,0,0,1,0.058,0.026 l0.086,0.096 A0.079,0.083,0,0,1,1,0.179 V0.917 c0,0.046,-0.035,0.083,-0.079,0.083 H0.079 c-0.044,0,-0.079,-0.037,-0.079,-0.083 V0.083 C0,0.037,0.035,0,0.079,0" />
+                </clipPath>
+              </svg>
             </div>
           ))}
         </div>
