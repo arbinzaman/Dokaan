79699b27cd30fb5efdbbc816e150330428f62c5f
Arbin Zaman <arbin16203@anwargroup.com>
2025-05-20 13:13:10 +0600

    style: fixed the style issue in product page

--- src/components/dashBoard/home/products/ProductsTable.jsx
+++ src/components/dashBoard/home/products/ProductsTable.jsx
@@ -34,7 +34,7 @@ const ProductsTable = ({ products }) => {
 
   return (
     <motion.div
-      className="bg-white dark:bg-black bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 mb-10"
+      className="bg-white dark:bg-black bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 mb-10 mt-10"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.2 }}
